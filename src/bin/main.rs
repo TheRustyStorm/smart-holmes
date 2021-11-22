a00c177074f21fd3e1cd1720669de4693c70d265
Peter <peter.zdankin@uni-due.de>
2021-11-22 11:25:04 +0100

    Matthias Sequences modification

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -12,7 +12,7 @@ fn generate_smart_home() -> SmartHome {
         amount_services: 50,
     };
     let device_config = DeviceConfig {
-        amount_devices: 50,
+        amount_devices: 5,
         services_per_device: 5,
     };
     let dependency_config = DependencyConfig {
@@ -37,87 +37,22 @@ fn generate_smart_home() -> SmartHome {
 }
 
 fn main() {
-    let mut sum_none = 0;
-    let mut sum_smart = 0;
-    let mut sum_all = 0;
-    let mut sum_random = 0;
-    for _ in 0..1000 {
-        let mut smart_home = generate_smart_home();
-        let amount_implicit_dependencies_by_user = 5;
-        let mut user = User::new(&smart_home, amount_implicit_dependencies_by_user);
-        let mut transition_matrix = TransitionMatrix::new(smart_home.devices.len());
-        let mut current_device;
-        for _ in 0..50 {
-            user.choose_random_new_device();
-            current_device = user.currently_used_device();
-            for _ in 0..100 {
-                user.step();
-                let next_device = user.currently_used_device();
-                if next_device != current_device {
-                    transition_matrix.increase(current_device, next_device);
-                }
-                current_device = next_device;
-            }
+   
+    let smart_home = generate_smart_home();
+    let amount_implicit_dependencies_by_user = 0;
+    let mut user = User::new(&smart_home, amount_implicit_dependencies_by_user);
+    println!("{:?}",user.model);
+    let mut transition_matrix = TransitionMatrix::new(smart_home.devices.len());
+    let mut current_device;
+    user.choose_random_new_device();
+    loop {
+        current_device = user.currently_used_device();
+        user.step();
+        let next_device = user.currently_used_device();
+        if next_device != current_device {
+            transition_matrix.increase(current_device, next_device);
         }
-        //transition_matrix.print();
-        for row in 0..smart_home.devices.len() {
-            let sum_of_row = transition_matrix.sum_at_row(row);
-            if sum_of_row > 10 {
-                for column in 0..smart_home.devices.len() {
-                    let value = transition_matrix.get_at(row, column);
-                    if value * 2 > sum_of_row {
-                        println!("Detected implicit dependency from {} to {}", row, column);
-                        let new_dependency = Dependency::new(
-                            vec![row, column],
-                            smart_home.get_device(column).services.clone(),
-                            smart_home.dependencies.len(),
-                        );
-                        smart_home.dependencies.push(new_dependency);
-                    }
-                }
-            }
-        }
-
-        println!(
-            "{} working dependencies",
-            smart_home.amount_fullfilled_dependencies()
-        );
-        let mut smart_home_all = smart_home.clone();
-        println!("Updating all");
-        smart_home_all.update_all();
-        println!(
-            "{} working dependencies \t Update Score: {}",
-            smart_home_all.amount_fullfilled_dependencies(),
-            smart_home_all.update_score()
-        );
-
-        let mut smart_home_random = smart_home.clone();
-        println!("Updating random");
-        smart_home_random.update_random();
-        println!(
-            "{} working dependencies \t Update Score: {}",
-            smart_home_random.amount_fullfilled_dependencies(),
-            smart_home_random.update_score()
-        );
-        sum_none += smart_home.update_score();
-        let mut smart_home_smart = smart_home;
-        println!("Updating Smart");
-        smart_home_smart.update_smart();
-        println!(
-            "{} working dependencies \t Update Score: {}",
-            smart_home_smart.amount_fullfilled_dependencies(),
-            smart_home_smart.update_score()
-        );
-
-       
-        sum_all += smart_home_all.update_score();
-        sum_random += smart_home_random.update_score();
-        sum_smart += smart_home_smart.update_score();
+        //println!("{}",current_device);
+        current_device = next_device;
     }
-    println!("{} {} {} {}", sum_smart/1000, sum_all/1000, sum_random/1000, sum_none/1000);
-    //println!("All: {} Smart: {}", sum_all/1000, sum_smart/1000);
-    
-    // let systems = Subsystem::find_subsystems(&mut smart_home);
-    // println!("{}", systems[0]);
-    // Subsystem::find_configurations(systems[0].clone());
 }
